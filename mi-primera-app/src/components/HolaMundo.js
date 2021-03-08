a736dbfe03ea4684da0b73649354e31d4e2dc112
= <mprada.ing@gmail.com>
2021-03-07 23:11:06 -0500

    priemros pasos react

---
+++ mi-primera-app/src/components/HolaMundo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function HolaMundo() {
+
+  return (
+    <div>
+      <h1> Hola mundo! </h1>
+    </div>
+  )
+}
+
+export function AdiosMundo() {
+
+  return (
+    <div>
+      <h2> ADios mundo! </h2>
+    </div>
+  )
+}
+
+export default HolaMundo;
